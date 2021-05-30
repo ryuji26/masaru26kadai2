67361fc87f351f5ce9027b8f66058021733a6db6
ryuji <ryujispjp@gmail.com>
2021-05-30 18:35:55 +0900

    guard文の書き方が手探り

--- kadai2/ViewController.swift
+++ kadai2/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var segmentedControl: UISegmentedControl!
+    @IBOutlet private weak var label: UILabel!
+
+    @IBAction func calculate(_ sender: Any) {
+        //textFieldの型を変換
+        let number1 = Int(textField1.text ?? "") ?? 0
+        let number2 = Int(textField2.text ?? "") ?? 0
+
+        //計算してlabelに表示
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            let result = number1 + number2
+            label.text = "\(result)"
+        case 1:
+            let result = number1 - number2
+            label.text = "\(result)"
+        case 2:
+            let result = number1 * number2
+            label.text = "\(result)"
+        case 3:
+            guard number2 != 0 else {
+                label.text = "割る数には0以外を入力してください"
+                return
+            }
+            let result = number1 / number2
+            label.text = "\(result)"
+        default:
+            return
+        }
     }
-
-
 }
-
